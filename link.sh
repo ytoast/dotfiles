2069f7654447882c65243a35b1104354af9ccec1
Brenda Thng <brenda.thng@foodpanda.com>
2021-10-11 23:47:35 +0800

    Added tmate and gitignore

--- link.sh
+++ link.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tmate.conf ~/.tmate.conf
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/aliases ~/.aliases
 ln -sf ~/dotfiles/vimrc ~/.vimrc
