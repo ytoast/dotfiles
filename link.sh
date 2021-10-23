1a6bdd653e154ceeb6f878438e9c80a6bdcf77e3
Brenda Thng <brenda.thng@foodpanda.com>
2021-10-23 15:30:41 +0800

    Add gitconfig file

--- link.sh
+++ link.sh
@@ -3,5 +3,6 @@ ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/tmate.conf ~/.tmate.conf
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/aliases ~/.aliases
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/alacritty.yml ~/.alacritty.yml
