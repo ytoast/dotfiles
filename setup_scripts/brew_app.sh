c81d738a06b14639ad0a01718dd32b8e6d75a45e
Brenda Thng <brenda.thng@foodpanda.com>
2022-03-04 20:30:39 +0800

    Add more scripts to install
    - Also updated aliases to move icons
    - Update deprecated field in alacritty.yml

--- setup_scripts/brew_app.sh
+++ setup_scripts/brew_app.sh
@@ -1,34 +1,43 @@
 #!/usr/bin/env bash
 
 # Misc casks
-# brew install --cask alfred
-# brew install --cask google-chrome
-# brew install --cask notion
-# brew install --cask spotify
-# brew install --cask obsidian
-# brew install --cask visual-studio-code
-# brew install --cask hiddenbar
-# brew install --cask rectangle
+brew install --cask alfred
+brew install --cask karabiner-elements
+brew install --cask google-chrome
+brew install --cask notion
+brew install --cask spotify
+brew install --cask obsidian
+brew install --cask visual-studio-code
+brew install --cask hiddenbar
+brew install --cask rectangle
+brew install --cask alacritty
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
 
 # Packages
-# brew install mas
-# brew install pre-commit
-# brew install gnupg
-# brew install gh
-# brew install entr
-# brew install tmuxinator
-# brew install jq
-# brew install fzf
-# brew install stow
+brew install asdf
+brew install entr
+brew install exa
+brew install fzf
+brew install gh
+brew install gnupg
+brew install jq
+brew install mas
+brew install neovim
+brew install pre-commit
+brew install rg
+brew install stow
+brew install tmate
+brew install tmuxinator
 
 # Install mac apps
-# mas install 1480933944  # Vimari
-# mas install 747648890   # Telegram
-# mas install 803453959   # Slack
-# mas install 1306893526    # Sorted3
-# mas install 1510445899    # Meeter
-# mas install 937984704     # Amphetamine
+mas install 1480933944  # Vimari
+mas install 747648890   # Telegram
+mas install 803453959   # Slack
+mas install 1306893526    # Sorted3
+mas install 1510445899    # Meeter
+mas install 937984704     # Amphetamine
 
 # Tap
-# brew tap hashicorp/tap
-# brew install hashicorp/tap/terraform
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -1,3 +1,14 @@
+# Install Brew
+# curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/bthng/.zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install ohmyzsh - don't do this
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install brew apps
+./brew_app.sh
+
 # Needed to create links
 sudo chown -R $(whoami) /usr/local/*
 
@@ -9,5 +20,5 @@ sh ~/dotfiles/macos.sh
 
 
 # Install AWS CLI
-curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-sudo installer -pkg AWSCLIV2.pkg -target /
+# curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+# sudo installer -pkg AWSCLIV2.pkg -target /
