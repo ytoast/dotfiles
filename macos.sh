23e186beed6185fd9ae0d5720976bc976b341eab
Brenda Thng <brenda@polychain.engineering>
2023-06-23 22:51:35 +0800

    Update macos defaults
    Added battery percentage as default
    Also rearranged lines for clarity

--- macos.sh
+++ macos.sh
@@ -1,13 +1,28 @@
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-# Place dock on the left
+# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Finder Enable text selection in quick look window
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE;killall Finder
+
+# Dock: Place dock on the left
 defaults write com.apple.dock "orientation" -string "left" && killall Dock
 
-# Automatically hide and show the Dock
+# Dock: Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
-# Save screenshots to the desktop
+# Control Center: Show battery percentage
+defaults write com.apple.controlcenter.plist BatteryShowPercentage -bool true
+
+# Screencapture: Save screenshots to the desktop
 mkdir "$HOME/Desktop/Screenshots"
 defaults write com.apple.screencapture location -string "$HOME/Desktop/Screenshots"
 
@@ -21,13 +36,6 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
-# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
-defaults write com.apple.finder QuitMenuItem -bool true
-
-# Finder: show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-# Finder: show path bar
-defaults write com.apple.finder ShowPathbar -bool true
-
+# Holding down a character key will repeat the character:
 defaults write -g ApplePressAndHoldEnabled -bool false
+
