a7c1fbfaa93a5262390058b39def479a2280b5ac
Brenda Thng <brenda.thng@messari.io>
2025-05-26 14:06:16 +0800

    Add envrc template
    Add this `ln -s ~/dotfiles/.envrc.template .envrc` to new repos where
    applicable

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -1,6 +1,7 @@
 --Treesitter
 local treesitter = require('nvim-treesitter.configs')
 treesitter.setup {
+  ensure_installed = { "sql" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
